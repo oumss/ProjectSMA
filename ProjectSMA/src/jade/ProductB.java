f14c4430eb604e69699d69376579c127592ffb5d
sariah8 <37137571+sariah8@users.noreply.github.com>
2020-12-13 20:15:57 +0100

    Update ProductB.java

--- ProjectSMA/src/jade/ProductB.java
+++ ProjectSMA/src/jade/ProductB.java
@@ -8,8 +8,8 @@ public class ProductB extends Product {
 	/**
 	 * The constructor of Product of type B
 	 * 
-	 * @param price    : the price of product B
-	 * @param quantity : the quantity of product B
+	 * @param price    : the price of B
+	 * @param quantity : the quantity of B
 	 */
 	public ProductB(double price, int quantity) {
 
