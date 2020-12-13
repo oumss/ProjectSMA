beb87cf735bb56276fe0863388a0d01ff567a1dd
sariah8 <37137571+sariah8@users.noreply.github.com>
2020-12-13 20:16:29 +0100

    Update ProductC.java

--- ProjectSMA/src/jade/ProductC.java
+++ ProjectSMA/src/jade/ProductC.java
@@ -8,8 +8,8 @@ public class ProductC extends Product {
 	/**
 	 * The constructor of Product of type C
 	 * 
-	 * @param price    : the price of product C
-	 * @param quantity : the quantity of product C
+	 * @param price    : the price of C
+	 * @param quantity : the quantity of C
 	 */
 	public ProductC(double price, int quantity) {
 
